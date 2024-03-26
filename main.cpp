a25613cde3c7e33ca86c4856cbe064342d588a0f
fadd00 <andhikahutama9@gmail.com>
2024-03-26 14:26:35 +0700

    membuat prosedur input data

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
+void inputData(){
+    cout << "masukan bilangan Pertama : ";
+    cin >> bilangan1;
+    cout << "masukan bilangan Kedua : ";
+    cin >> bilangan2;
+}
 
 int main() {
 
